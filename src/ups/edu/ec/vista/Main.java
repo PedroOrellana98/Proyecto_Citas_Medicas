1d191544bd19aebae5198315400347f287eae773
Pedro Orellana <porellanaj@est.ups.edu.ec>
2021-01-06 21:35:56 -0500

    Commit librerias
    Actualizacion de librerias

---
+++ src/ups/edu/ec/vista/Main.java
@@ -0,0 +1,11 @@
+package ups.edu.ec.vista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		index i = new index();
+		i.setVisible(true);
+	}
+
+}
